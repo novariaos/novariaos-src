271c9b3b2ee2d4581fa9e355ea0d43f73fe4ae04
Zennix <zennix@tuta.io>
2026-02-19 23:24:57 +0300

    feat: add hotkey reg & unreg

--- include/core/drivers/keyboard.h
+++ include/core/drivers/keyboard.h
@@ -5,7 +5,12 @@
 #include <stdbool.h>
 
 #define KEYBOARD_BUFFER_SIZE 256
+#define HOTKEY_MOD_SHIFT 1
+#define HOTKEY_MOD_CTRL  2
+#define HOTKEY_MOD_ALT   4
 
+int keyboard_register_hotkey(int scancode, int modifiers, void (*callback)(void*), void* data);
+void keyboard_unregister_hotkey(int id);
 void keyboard_init(void);
 char keyboard_getchar(void);
 bool keyboard_has_char(void);
